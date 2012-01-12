53328580a8a12ac3b8805fe47221db5599d5193f
papousek <jan.papousek@gmail.com>
2012-01-12 12:13:06 +0100

    pom test

--- src/test/java/org/richfaces/hudson/SystemPropertyTest.java
+++ src/test/java/org/richfaces/hudson/SystemPropertyTest.java
@@ -2,20 +2,21 @@ package org.richfaces.hudson;
 
 import junit.framework.TestCase;
 
-public class SystemPropertyTest  extends TestCase
-{
+public class SystemPropertyTest  extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public SystemPropertyTest( String testName )
-    {
+    public SystemPropertyTest( String testName ) {
         super( testName );
     }
 
-    public void testApp()
-    {
+    public void testArgLine() {
         assertNotNull("System property <test.property> is not set.", System.getProperty("test.property"));
+    }
+    
+    public void testPom() {
+        assertNotNull("System property <test.pom.property> is not set.", System.getProperty("test.pom.property"));
     }
 }
